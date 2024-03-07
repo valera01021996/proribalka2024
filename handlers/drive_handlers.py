bbcc74e416c2e70519ccdbcce5b11851521b74fd
valera <valerich919@ya.ru>
2024-03-07 14:11:50 +0500

    otkorrektiroval vivod soobweniya o nakopitele

--- handlers/drive_handlers.py
+++ handlers/drive_handlers.py
@@ -9,8 +9,9 @@ async def show_bonus(message: Message):
     chat_id = message.chat.id
     phone_number = DBTools().drive_tools.select_phone_number_of_user(chat_id)
     try:
-        bonus = DBTools().drive_tools.select_bonus_of_user(phone_number)
-        await bot.send_message(chat_id, f"У вас накопилось : {bonus}")
+        bonus = int(DBTools().drive_tools.select_bonus_of_user(phone_number))
+        formatted_bonus = f"{bonus:,}".replace(",", " ")
+        await bot.send_message(chat_id, f"У вас накопилось : {formatted_bonus} баллов \nНапоминаем: 1 балл = 1 сум")
     except Exception as ex:
         print(ex)
         await bot.send_message(chat_id, "Нет бонусов по вашему номеру телефона.")
